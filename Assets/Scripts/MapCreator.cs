0f93f33413e24ba6f0f46b6c0c3ca4f0a8331799
DelightLane <hano881112@hanmir.com>
2016-03-08 00:56:26 +0900

    MapCreator 개선(타일들이 root를 가지게 변경)

--- Assets/Scripts/MapCreator.cs
+++ Assets/Scripts/MapCreator.cs
@@ -5,6 +5,8 @@ public class MapCreator
 {
     private static MapCreator _instance;
 
+    private readonly float TILE_SIZE = 1;
+
     public static MapCreator Instance
     {
         get
@@ -24,14 +26,30 @@ public class MapCreator
     {
         var tile = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
-        tile.transform.localScale = new Vector3(1, 0.05f, 1);
+        tile.transform.localScale = new Vector3(TILE_SIZE, 0.05f, TILE_SIZE);
 
         return tile;
     }
 
     public void CreateTilemap(Vector2 mapSize)
     {
-        var offset = 1.01f;
+        var root = GameObject.Find("Tilemap");
+        if(root == null)
+        {
+            root = new GameObject("Tilemap");
+            root.transform.position = Vector3.up * -1;
+        }
+        else
+        {
+            var rootChilds = root.GetComponentsInChildren<GameObject>();
+
+            foreach(var child in rootChilds)
+            {
+                GameObject.Destroy(child);
+            }
+        }
+
+        var offset = 0.03f;
 
         for (int i = 0; i < mapSize.x; ++i)
         {
@@ -39,7 +57,9 @@ public class MapCreator
             {
                 var tile = CreateTile();
 
-                tile.transform.position = new Vector3(i * offset, -1, j * offset);
+                tile.transform.position = new Vector3(i * (TILE_SIZE + offset), 0, j * (TILE_SIZE + offset));
+
+                tile.transform.parent = root.transform;
             }
         }
     }
