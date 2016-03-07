44d9da91694414face8339c949a0e366a60f563b
DelightLane <hano881112@hanmir.com>
2016-03-08 00:40:14 +0900

    MapCreate 싱글턴 구축 및 InitializeManager 프리팹 추가

---
+++ Assets/Scripts/InitializeManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitializeManager : MonoBehaviour {
+    [SerializeField]
+    private Vector2 _mapSize;
+
+	void Awake()
+    {
+        MapCreator.Instance.CreateTilemap(_mapSize);
+    }
+}
---
+++ Assets/Scripts/MapCreator.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapCreator
+{
+    private static MapCreator _instance;
+
+    public static MapCreator Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = new MapCreator();
+            }
+
+            return _instance;
+        }
+    }
+
+    private MapCreator() { }
+
+    private GameObject CreateTile()
+    {
+        var tile = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        tile.transform.localScale = new Vector3(1, 0.05f, 1);
+
+        return tile;
+    }
+
+    public void CreateTilemap(Vector2 mapSize)
+    {
+        var offset = 1.01f;
+
+        for (int i = 0; i < mapSize.x; ++i)
+        {
+            for(int j = 0; j < mapSize.y; ++j)
+            {
+                var tile = CreateTile();
+
+                tile.transform.position = new Vector3(i * offset, -1, j * offset);
+            }
+        }
+    }
+}
