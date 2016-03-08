e4e94920d70c14ad14bedd2608727e5fa9225a16
DelightLane <hano881112@hanmir.com>
2016-03-09 00:28:56 +0900

    게임 오브젝트들이 가질 기본 스크립트의 상위 추상 클래스 및 그를 상속받는 타일 클래스 제작

---
+++ Assets/Scripts/Objects/TacticsObject.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 본 게임 내부의 모든 오브젝트 컨트롤 클래스들의 최상위 부모 클래스
+public abstract class TacticsObject : MonoBehaviour {
+    
+}
---
+++ Assets/Scripts/Objects/Tile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : TacticsObject {
+    private TacticsObject _attached;
+
+    public TacticsObject GetAttachObject()
+    {
+        return _attached;
+    }
+
+    public void AttachObject(TacticsObject obj)
+    {
+        _attached = obj;
+    }
+}
