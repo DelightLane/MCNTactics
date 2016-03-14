3f5519a1d9b13c5bd8a604acc3b887f3add12d62
DelightLane <hano881112@hanmir.com>
2016-03-15 01:56:34 +0900

    State 추상 클래스 및 StateMachine 클래스 추가

---
+++ Assets/Scripts/HelperClass/State.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCN
+{
+    public abstract class State
+    {
+        private TacticsObject _target;
+        protected TacticsObject Target {
+            get
+            {
+                return _target;
+            }
+        }
+
+        public State(TacticsObject target)
+        {
+            _target = target;
+
+            Initialize();
+        }
+
+        public virtual void Initialize() { }
+        public virtual void Destroy() { }
+
+        public abstract void Run();
+        public virtual void Finish() { }
+    }
+
+    public class StateMachine<T> where T : State
+    {
+        private Dictionary<string, T> _states = new Dictionary<string, T>();
+        private T _currentState;
+
+        public void StorageState(string stateName, T state)
+        {
+            _states.Add(stateName, state);
+        }
+
+        public void ChangeState(string stateName)
+        {
+            if(_currentState != null)
+            {
+                _currentState.Finish();
+            }
+
+            _currentState = _states[stateName];
+
+            if(_currentState != null)
+            {
+                _currentState.Run();
+            }
+        }
+    }
+}
