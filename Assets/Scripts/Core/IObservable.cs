76e9a94a14b1b202868358f9270b007b5520417d
DelightLane <hano881112@hanmir.com>
2016-03-19 00:04:26 +0900

    IObservable, IObserver, TouchManager 추가

---
+++ Assets/Scripts/Core/IObservable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace MCN
+{
+    public interface IObservable<T>
+    {
+        void Subscribe(IObserver<T> observer);
+        void Unsubscribe(IObserver<T> observer);
+    }
+
+    public interface IObserver<T>
+    {
+        void OnNext(T data);
+    }
+}
---
+++ Assets/Scripts/Manager/TouchManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using MCN;
+
+// TODO : 더 많은 터치 이벤트들을 구현해야 함
+public enum eTouchEvent
+{
+    TOUCH
+}
+
+public class TouchManager : MonoSingletone<TouchManager>, IObservable<eTouchEvent>
+{
+    private List<IObserver<eTouchEvent>> _observers;
+
+    void Awake()
+    {
+        _observers = new List<IObserver<eTouchEvent>>();
+    }
+
+    void Update()
+    {
+        // TODO : 마우스가 아니라 실제로 터치 이벤트에 대해 동작하게 수정
+        if (Input.GetMouseButtonUp(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                foreach (var observer in _observers)
+                {
+                    var gameObjObserver = observer as MonoBehaviour;
+                    if (gameObjObserver != null)
+                    {
+                        if (gameObjObserver.transform == hit.transform)
+                        {
+                            observer.OnNext(eTouchEvent.TOUCH);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void Subscribe(IObserver<eTouchEvent> observer)
+    {
+        if (_observers != null)
+        {
+            _observers.Add(observer);
+        }
+    }
+
+    public void Unsubscribe(IObserver<eTouchEvent> observer)
+    {
+        if (_observers != null)
+        {
+            _observers.Remove(observer);
+        }
+    }
+
+    protected override string CreatedObjectName()
+    {
+        return "TouchManager";
+    }
+
+}
