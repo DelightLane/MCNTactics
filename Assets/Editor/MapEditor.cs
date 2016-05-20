f4349b93c38642bfb09a7dca0609a475de148934
DelightLane <hano881112@hanmir.com>
2016-05-20 23:42:30 +0900

    맵 툴 업데이트(텍스쳐 이미지를 타일 아틀라스에서 잘라서 쓸 수 있게 수정)

--- Assets/Editor/MapEditor.cs
+++ Assets/Editor/MapEditor.cs
@@ -2,17 +2,23 @@
 using UnityEditor;
 using System.Collections.Generic;
 
+// 픽셀을 다루기 때문에 아틀라스 텍스쳐는 reabable이어야 한다.
 public class MapEditor : EditorWindow
 {
     private string _mapName;
     private Vector2 _mapSize;
 
-    private List<Texture2D> _tileData = new List<Texture2D>();
+    private Texture2D _tempTile;
 
-    private readonly float MAP_X = 300;
-    private readonly float MAP_Y = 0;
+
+    private string _resultMapName;
+    private Vector2 _resultMapSize;
+    private Texture2D[] _tileData;
+
+    private readonly float MAP_START_X = 300;
+    private readonly float MAP_START_Y = 0;
     private readonly float MAP_MARGIN = 3;
-    private readonly float MAP_SIZE = 80;
+    private readonly float MAP_SIZE = 40;
 
     [MenuItem("Window/FZTools/MapEditor")]
     static void Init()
@@ -22,6 +28,8 @@ public class MapEditor : EditorWindow
 
     void OnGUI()
     {
+        _tempTile = Resources.Load("Atlases/TileAtlas", typeof(Texture2D)) as Texture2D;
+
         GUILayout.BeginHorizontal();
 
         DisplaySettingBox();
@@ -33,13 +41,31 @@ public class MapEditor : EditorWindow
 
     private void DisplaySettingBox()
     {
-        GUILayout.BeginVertical("Box", GUILayout.Width(MAP_X));
+        GUILayout.BeginVertical("Box", GUILayout.Width(MAP_START_X));
 
         _mapName = EditorGUILayout.TextField("Map Name: ", _mapName);
         _mapSize = EditorGUILayout.Vector2Field("Map Size", _mapSize);
+
         if (GUILayout.Button("New"))
         {
-            _tileData.Add(Resources.Load("Images/tile0", typeof(Texture2D)) as Texture2D);
+            int mapMaxSize = (int)_mapSize.x * (int)_mapSize.y;
+
+            _tileData = new Texture2D[mapMaxSize];
+
+            for (int y = 0; y < _mapSize.y; ++y)
+            {
+                for(int x = 0; x < _mapSize.x; ++x)
+                {
+                    int position = x + (y * (x + 1));
+
+                    // TODO : 타일 데이터를 가져와서 아틀라스에서 해당 부분을 자르게 수정
+                    _tileData[position] = new Texture2D(32, 32);
+                    _tileData[position].SetPixels(_tempTile.GetPixels(0, 0, 32, 32));
+                    _tileData[position].Apply(false);
+                }
+            }
+
+            _resultMapSize = _mapSize;
         }
         if (GUILayout.Button("Save"))
         {
@@ -56,13 +82,28 @@ public class MapEditor : EditorWindow
     private void DisplayMapData()
     {
         // TODO : 제대로 유닛과 맵이 보이게 수정
-        for (int i = 0; i < _tileData.Count; ++i)
+        if (_resultMapSize != null)
         {
-            GUIStyle style = new GUIStyle();
-            style.normal.background = _tileData[i];
-            style.alignment = TextAnchor.MiddleCenter;
-            GUIContent content = new GUIContent(Resources.Load("images/tile1", typeof(Texture2D)) as Texture2D);
-            EditorGUI.LabelField(new Rect(MAP_X + (i + 1) * MAP_MARGIN + i * MAP_SIZE, MAP_Y + MAP_MARGIN, MAP_SIZE, MAP_SIZE), content, style);
+            for (int y = 0; y < _resultMapSize.x; ++y)
+            {
+                for (int x = 0; x < _resultMapSize.y; ++x)
+                {
+                    int position = x + (y * (x + 1));
+
+                    GUIStyle style = new GUIStyle();
+
+                    style.normal.background = _tileData[position];
+                    style.alignment = TextAnchor.MiddleCenter;
+                    GUIContent content = new GUIContent(Resources.Load("images/tile1", typeof(Texture2D)) as Texture2D);
+                    EditorGUI.LabelField(new Rect(MAP_START_X + (x + 1) * MAP_MARGIN + x * MAP_SIZE,
+                                                  MAP_START_Y + (y + 1) * MAP_MARGIN + y * MAP_SIZE,
+                                                  MAP_SIZE,
+                                                  MAP_SIZE),
+                                         content,
+                                         style);
+
+                }
+            }
         }
     }
 }
