5d8371cda5e63e903c0d6038841dd1b1be442d9c
DelightLane <hano881112@hanmir.com>
2016-05-25 01:35:12 +0900

    유닛을 선택했을 때만 ui 메뉴가 나타나게 수정

--- Assets/Scripts/GUI/TestUIController.cs
+++ Assets/Scripts/GUI/TestUIController.cs
@@ -2,7 +2,30 @@
 using System.Collections;
 
 public class TestUIController : MonoBehaviour
-{ 
+{
+    private GameObject _moveBtn;
+    private GameObject _atkBtn;
+
+    void Start()
+    {
+        _moveBtn = GameObject.Find("MoveBtn");
+        _atkBtn = GameObject.Find("AttackBtn");
+    }
+
+    void Update()
+    {
+        if(GameManager.Instance.SelectedObj == null)
+        {
+            _moveBtn.SetActive(false);
+            _atkBtn.SetActive(false);
+        }
+        else
+        {
+            _moveBtn.SetActive(true);
+            _atkBtn.SetActive(true);
+        }
+    }
+
     public void MoveTest()
     {
         var selected = GameManager.Instance.SelectedObj as UnitObject;
