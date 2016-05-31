1795574f006f7969554bcddcaf624e7efd2c9a11
DelightLane <hano881112@hanmir.com>
2016-06-01 00:02:05 +0900

    기본적인 카메라 움직임 동작 정의

---
+++ Assets/Scripts/Manager/CameraManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CameraManager : FZ.MonoSingletone<CameraManager>
+{
+    public float _moveSpeed = 0.2f;
+
+    protected override string CreatedObjectName()
+    {
+        return "CameraManager";
+    }
+
+    void Start()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(45, 0, 0));
+    }
+
+    void Update()
+    {        
+        if (Input.mousePosition.x > Screen.width - 1)
+        {
+            transform.Translate(Vector3.right * _moveSpeed);
+        }
+        if (Input.mousePosition.x < 1)
+        {
+            transform.Translate(-Vector3.right * _moveSpeed);
+        }
+        if (Input.mousePosition.y > Screen.height - 1)
+        {
+            transform.position = transform.position + Vector3.forward * _moveSpeed;
+        }
+        if (Input.mousePosition.y < 1)
+        {
+            transform.position = transform.position - Vector3.forward * _moveSpeed;
+        }
+    }
+}
