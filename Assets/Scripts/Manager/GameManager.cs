ca804b10d58b3e6d7b2a6f7171e4799baca01dae
DelightLane <hano881112@hanmir.com>
2016-06-03 00:22:42 +0900

    GameManager 리팩토링(SelecHandler로 기능 분리)

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -3,47 +3,77 @@ using System.Collections;
 
 public class GameManager : FZ.Singletone<GameManager>
 {
-    private TacticsObject _selectedObj;
+    private SelectHandler _selectHandler;
+
+    private GameManager()
+    {
+        _selectHandler = new SelectHandler();
+    }
+
+    #region SelectHandler
+    private class SelectHandler
+    {
+        private TacticsObject _selectedObj;
+
+        public void Set(TacticsObject obj)
+        {
+            _selectedObj = obj;
+        }
+
+        public TacticsObject Get()
+        {
+            return _selectedObj;
+        }
+
+        public void Action(System.Type actType)
+        {
+            var selected = _selectedObj as ActionObject;
+
+            if (selected != null)
+            {
+                selected.EnqueueActor(actType);
+            }
+        }
+
+        public void CancelAction()
+        {
+            var selected = _selectedObj as ActionObject;
+
+            if (selected != null)
+            {
+                if (selected.HasActor())
+                {
+                    selected.DequeueActor();
+                }
+                else
+                {
+                    selected.Deselect();
+                }
+            }
+        }
+    }
 
     public TacticsObject SelectedObj
     {
         get
         {
-            return _selectedObj;
+            return _selectHandler.Get();
         }
 
         set
         {
-            _selectedObj = value;
+            _selectHandler.Set(value);
         }
     }
 
-    private GameManager() { }
-
     public void Action(System.Type actType)
     {
-        var selected = GameManager.Instance.SelectedObj as ActionObject;
-
-        if (selected != null)
-        {
-            selected.EnqueueActor(actType);
-        }
+        _selectHandler.Action(actType);
     }
 
     public void CancelAction()
     {
-        var selected = GameManager.Instance.SelectedObj as ActionObject;
-
-        if (selected != null)
-        {
-            if (selected.HasActor())
-            {
-                selected.DequeueActor();
-            }
-            else
-            {
-                selected.Deselect();
-            }
-        }
+        _selectHandler.CancelAction();
     }
+    #endregion
 }
